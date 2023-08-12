f511a0baa4080e626fe1efb8127c4497fc313a9b
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2023-08-13 00:07:35 +0900

    万葉のジャンプを微調整、柱が一定時間おきに出現するように
    ・柱の削除処理が未実装

---
+++ Assets/PillarEmmiter.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Flappy.Gimmicks
+{
+	/// <summary>
+	/// 柱エミッター
+	/// </summary>
+	public class PillarEmmiter : MonoBehaviour
+	{
+		[SerializeField]
+		GameObject pillarPrefab;
+
+		[SerializeField]
+		float span;
+
+		float currentTime = 0f;
+
+		[SerializeField]
+		float pillarSpeed;
+
+		[SerializeField]
+		float pillarYRandomRange;
+
+		void Update()
+		{
+			this.currentTime += Time.deltaTime;
+			if( this.currentTime >= span )
+			{
+				this.currentTime = 0f;
+
+				var randY = Random.Range(-pillarYRandomRange, pillarYRandomRange);
+				var pillarPosition = this.transform.position + (Vector3.up * randY);
+				
+
+				var pillar = GameObject.Instantiate(this.pillarPrefab, pillarPosition, Quaternion.identity);
+				pillar.GetComponent<Rigidbody2D>().velocity = Vector2.left * this.pillarSpeed;
+			}
+		}
+	}
+}
