4d7dfc22bde7dca3c3c722274253c6a5172cb7c0
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2023-08-13 20:24:28 +0900

    端までいった柱を消すように

---
+++ Assets/App/Scripts/Gimmicks/Pillar.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Flappy.Gimmicks
+{
+	/// <summary>
+	/// 柱
+	/// </summary>
+	public class Pillar : MonoBehaviour
+	{
+		/// <summary>
+		/// 他のトリガコライダと接触した
+		/// </summary>
+		/// <param name="collider">接触対象のコライダ</param>
+		void OnTriggerEnter2D(Collider2D collider)
+		{
+			var otherLayer = collider.gameObject.layer;
+			var otherLayerName = LayerMask.LayerToName(otherLayer);
+
+			switch (otherLayerName)
+			{
+				case "PillarDestroyer":
+					{
+						GameObject.Destroy(this.gameObject);
+						break;
+					}
+				default:
+					{
+						// Nothing to do.
+						break;
+					}
+			}
+		}
+	}
+}
---
+++ Assets/App/Scripts/Gimmicks/PillarDestroyer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Flappy.Gimmicks
+{
+	public class PillarDestroyer : MonoBehaviour
+	{
+		[SerializeField]
+		BoxCollider2D collider;
+
+		void FixedUpdate()
+		{
+			
+		}
+	}
+
+}
