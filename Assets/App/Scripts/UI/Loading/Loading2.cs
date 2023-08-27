6f67959bbce0e46834d7589c527a02bf5ca272e7
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2023-08-28 00:48:42 +0900

    全画面ロードプレハブにフェードイン/アウト処理を追加
    ・まだ呼び出す側を作っていないので、LoadingManagerから呼び出すようにする必要がある

--- Assets/App/Scripts/UI/Loading/Loading2.cs
+++ Assets/App/Scripts/UI/Loading/Loading2.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,6 +11,37 @@ namespace Flappy.UI
 	{
 		[SerializeField]
 		Image progressBarFill;
+
+		[SerializeField]
+		CanvasGroup canvasGroup;
+
+		const float defaultFadeTime = 0.4f;
+
+		/// <summary>
+		/// ロード画面を表示する
+		/// </summary>
+		/// <param name="fadeTime">フェードイン時間</param>
+		/// TODO: 必要に応じてコールバックデリゲートを実装する
+		public void Show(float fadeTime = Loading2.defaultFadeTime)
+		{
+			this.gameObject.SetActive(true);
+			this.canvasGroup.alpha = 0f;
+			this.canvasGroup.DOFade(1f, fadeTime);
+		}
+
+		/// <summary>
+		/// ロード画面を非表示にする
+		/// </summary>
+		/// <param name="fadeTime">フェードアウト時間</param>
+		/// TODO: 必要に応じてコールバックデリゲートを実装する
+		public void Hide(float fadeTime = Loading2.defaultFadeTime)
+		{
+			this.canvasGroup.alpha = 1f;
+			this.canvasGroup.DOFade(0f, fadeTime).OnComplete(() =>
+			{
+				this.gameObject.SetActive(false);
+			});
+		}
 
 		/// <summary>
 		/// プログレスバーの進捗をセット
