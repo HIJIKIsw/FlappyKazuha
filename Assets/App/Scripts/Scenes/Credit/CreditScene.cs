51faa66227443c3c84c0f84022758f8bc461c02f
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2023-08-29 01:56:19 +0900

    Creditシーンを仮追加

---
+++ Assets/App/Scripts/Scenes/Credit/CreditScene.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Flappy
+{
+	public class CreditScene : MonoBehaviour, ISceneBase
+	{
+		public static string Name
+		{
+			get
+			{
+				return nameof(CreditScene);
+			}
+		}
+	}
+}
