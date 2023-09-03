02b0f808ddc5bc51dff56f589cd3818dc6657849
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2023-09-03 16:47:53 +0900

    定数管理用クラス追加

---
+++ Assets/App/Scripts/Common/Constants.cs
@@ -0,0 +1,29 @@
+namespace Flappy.Common
+{
+	/// <summary>
+	/// 定数管理クラス
+	/// </summary>
+	public static class Constants
+	{
+		/// <summary>
+		/// アセットアドレス
+		/// </summary>
+		public static class Assets
+		{
+			/// <summary>
+			/// 音アセット
+			/// </summary>
+			public static class Audio
+			{
+				/// <summary>
+				/// Sound Effect
+				/// </summary>
+				public enum SE
+				{
+					pico22
+				}
+			}
+		}
+
+	}
+}
