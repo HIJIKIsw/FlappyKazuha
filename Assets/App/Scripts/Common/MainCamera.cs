f71df8de690933469d3555dce3b054336fe36092
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2023-09-05 02:31:34 +0900

    メインカメラと代替カメラの切り替え前にSceneManagerの初期化を待機するように

--- Assets/App/Scripts/Common/MainCamera.cs
+++ Assets/App/Scripts/Common/MainCamera.cs
@@ -1,20 +1,55 @@
 using System.Collections;
-using System.Collections.Generic;
-using Flappy.Manager;
 using UnityEngine;
+using UnityEngine.Events;
+using Flappy.Manager;
+using Flappy.Utility;
 
 namespace Flappy.Common
 {
 	public class MainCamera : MonoBehaviour
 	{
+		// TODO: もうちょいきれいに書きたい
 		void OnEnable()
 		{
-			SceneManager.Instance.SetAlternativeCameraActive(false);
+			if (SceneManager.IsInitialized == false)
+			{
+				StaticCoroutine.Start(this.WaitForSceneManager(() =>
+				{
+					SceneManager.Instance.SetAlternativeCameraActive(false);
+				}));
+			}
+			else
+			{
+				SceneManager.Instance.SetAlternativeCameraActive(false);
+			}
 		}
 
+		// TODO: もうちょいきれいに書きたい
 		void OnDisable()
 		{
-			SceneManager.Instance.SetAlternativeCameraActive(true);
+			if (SceneManager.IsInitialized == false)
+			{
+				StaticCoroutine.Start(this.WaitForSceneManager(() =>
+				{
+					SceneManager.Instance.SetAlternativeCameraActive(true);
+				}));
+			}
+			else
+			{
+				SceneManager.Instance.SetAlternativeCameraActive(true);
+			}
+		}
+
+		/// <summary>
+		/// シーンマネージャのインスタンスが作られるまで待機
+		/// </summary>
+		IEnumerator WaitForSceneManager(UnityAction callback)
+		{
+			while (SceneManager.IsInitialized == false)
+			{
+				yield return null;
+			}
+			callback?.Invoke();
 		}
 	}
 }
