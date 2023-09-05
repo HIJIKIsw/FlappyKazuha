bec536e11b39b8ddbd3b5432b88f1bc432f58c89
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2023-09-06 02:34:31 +0900

    ゲーム挙動に関わるメソッド追加

--- Assets/App/Scripts/Scenes/PlayGame/PlayGameScene.cs
+++ Assets/App/Scripts/Scenes/PlayGame/PlayGameScene.cs
@@ -1,6 +1,6 @@
-using System.Collections;
 using System.Collections.Generic;
 using Flappy.Common;
+using Flappy.Gimmicks;
 using UnityEngine;
 
 namespace Flappy
@@ -8,5 +8,36 @@ namespace Flappy
 	public class PlayGameScene : SceneBase
 	{
 		public override string Name => "PlayGame";
+
+		[SerializeField]
+		GameObject pillarContainer;
+
+		[SerializeField]
+		PillarEmmiter pillarEmmiter;
+
+		public void GameOver()
+		{
+			var pillars = this.GetAllPillars();
+			foreach (var pillar in pillars)
+			{
+				pillar.SetSpeed(Vector2.zero);
+			}
+			this.pillarEmmiter.gameObject.SetActive(false);
+		}
+
+		public List<Pillar> GetAllPillars()
+		{
+			var pillars = new List<Pillar>();
+			var pillarCount = this.pillarContainer.transform.childCount;
+			for (int i = 0; i < pillarCount; i++)
+			{
+				var pillar = this.pillarContainer.transform.GetChild(i).GetComponent<Pillar>();
+				if (pillar != null)
+				{
+					pillars.Add(pillar);
+				}
+			}
+			return pillars;
+		}
 	}
 }
