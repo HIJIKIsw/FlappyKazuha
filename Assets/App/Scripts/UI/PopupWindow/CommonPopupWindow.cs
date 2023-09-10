1b6af8db70175d514c32f6349f346e1e970fb90a
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2023-09-10 23:46:53 +0900

    汎用ポップアップウィンドウ用クラスを作成してアタッチ

---
+++ Assets/App/Scripts/UI/PopupWindow/CommonPopupWindow.cs
@@ -0,0 +1,21 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Flappy.UI
+{
+	public class CommonPopupWindow : MonoBehaviour
+	{
+		[SerializeField]
+		Button button1;
+
+		[SerializeField]
+		Button button2;
+
+		[SerializeField]
+		TextMeshProUGUI title;
+
+		[SerializeField]
+		TextMeshProUGUI message;
+	}
+}
