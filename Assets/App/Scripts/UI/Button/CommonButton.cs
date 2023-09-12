bd0e85a468d2a1b69ed99a7b1c4b34109027377b
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2023-09-13 00:13:29 +0900

    クリック時のアクション登録機能を追加

--- Assets/App/Scripts/UI/Button/CommonButton.cs
+++ Assets/App/Scripts/UI/Button/CommonButton.cs
@@ -1,7 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 namespace Flappy.UI
@@ -24,6 +23,12 @@ namespace Flappy.UI
 		TextMeshProUGUI label;
 
 		/// <summary>
+		/// ボタンコンポーネント
+		/// </summary>
+		[SerializeField]
+		Button button;
+
+		/// <summary>
 		/// 初期化処理
 		/// </summary>
 		private void Start()
@@ -36,6 +41,7 @@ namespace Flappy.UI
 		/// アイコン画像をセット
 		/// </summary>
 		/// <remarks>メソッドチェーン可</remarks>
+		// TODO: Sprite渡すんじゃなくてアイコン指定する定数を渡すようにする
 		public CommonButton SetIcon(Sprite icon)
 		{
 			// アイコンの画像をセット
@@ -55,6 +61,20 @@ namespace Flappy.UI
 		{
 			// ボタンの文言をセット
 			this.label.text = label;
+
+			return this;
+		}
+
+		/// <summary>
+		/// クリック時のアクションをセット
+		/// </summary>
+		public CommonButton SetClickAction(UnityAction action)
+		{
+			// 既存のアクションを削除
+			this.button.onClick.RemoveAllListeners();
+
+			// アクションを登録
+			this.button.onClick.AddListener(action);
 
 			return this;
 		}
