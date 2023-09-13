fc7d2cb9dfcab860ceb83c67cb8aa89afd0b2a96
shulapowerman <shulapowerman@gmail.com>
2023-09-14 02:07:29 +0900

    インデント修正

--- Assets/App/Scripts/Gimmicks/Ground/Ground.cs
+++ Assets/App/Scripts/Gimmicks/Ground/Ground.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 namespace Flappy.Gimmicks
 {
-    /// <summary>
+	/// <summary>
 	/// 地面
 	/// </summary>
 	public class Ground : MonoBehaviour
 	{
 		[SerializeField]
 		new Rigidbody2D rigidbody2D;
-		
+
 		/// <summary>
 		/// 地面を消す処理
 		/// </summary>
