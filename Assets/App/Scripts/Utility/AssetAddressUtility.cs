feea07d78dd1c07ab073cc866c1850f46819f4e7
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2023-09-24 16:14:05 +0900

    アセットアドレスを取得するためのユーティリティクラスを追加

---
+++ Assets/App/Scripts/Utility/AssetAddressUtility.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+using Flappy.Common;
+using System.Collections.Generic;
+
+namespace Flappy.Utility
+{
+	/// <summary>
+	/// Addressablesからアセットを取得するためのユーティリティ
+	/// </summary>
+	public class AssetAddressUtility
+	{
+		/// <summary>
+		/// アセットタイプごとのディレクトリ定義
+		/// </summary>
+		private static readonly Dictionary<Type, string> directoryMap = new()
+		{
+			{typeof(Constants.Assets.Sprite.ButtonIcon),            "Sprite/ButtonIcon/"},
+			{typeof(Constants.Assets.Audio.SE),                     "Audio/SE/"},
+		};
+
+		/// <summary>
+		/// Addressablesのアセットアドレスを取得
+		/// </summary>
+		/// <param name="asset">アセットの種類<para>Constants.Assets.* 配下のenum定数を渡す</para></param>
+		public static string GetAssetAddress(Enum asset)
+		{
+			Type assetType = asset.GetType();
+
+			string directory;
+			if (AssetAddressUtility.directoryMap.TryGetValue(assetType, out directory) == false)
+			{
+				Debug.LogError($"未知のアセットタイプが指定されました。アセットタイプ: {assetType}");
+				return null;
+			}
+
+			return directory + asset.ToString();
+		}
+	}
+}
