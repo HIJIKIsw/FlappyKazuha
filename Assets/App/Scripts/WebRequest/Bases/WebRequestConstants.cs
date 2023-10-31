55bdd0fb1b4c61e217ff324c54bccaab15e592cd
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2023-11-01 02:01:01 +0900

    WebRequestConstantsをオーバーライドする仕組みを作った
    - 各環境のAPI設定はバージョン管理対象外として各作業者のローカル環境にのみ配置する

--- Assets/App/Scripts/WebRequest/Bases/WebRequestConstants.cs
+++ Assets/App/Scripts/WebRequest/Bases/WebRequestConstants.cs
@@ -1,53 +1,62 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using System.Reflection;
 using UnityEngine;
 
 namespace Flappy.WebRequest
 {
 	/// <summary>
-	/// WebRequstで使う定数
+	/// WebRequestで使用する定数
 	/// </summary>
 	public class WebRequestConstants
 	{
-#if ENV_DEVELOPMENT
+		// WebRequestConstantsOverrideで上書きすることを前提にしている
 		/// <summary>
-		/// APIのURL (開発)
+		/// APIのURL
 		/// </summary>
-		public const string ApiHttpRoot = "http://hogehoge.dev/";
-		/// <summary>
-		/// APIの認証キー
-		/// </summary>
-		public const string ApiAuthKey = "hogehogehoge";
-		/// <summary>
-		/// APIのボディ暗号化キー
-		/// </summary>
-		public const string ApiBodyEncryptKey = "fugafugafuga";
-#elif ENV_TEST
-		/// <summary>
-		/// APIのURL (テスト)
-		/// </summary>
-		public const string ApiHttpRoot = "http://hogehoge.stg/";
+		public static string ApiHttpRoot { private set; get; } = string.Empty;
 		/// <summary>
 		/// APIの認証キー
 		/// </summary>
-		public const string ApiAuthKey = "hogehogehoge";
+		public static string ApiAuthKey { private set; get; } = string.Empty;
 		/// <summary>
 		/// APIのボディ暗号化キー
 		/// </summary>
-		public const string ApiBodyEncryptKey = "fugafugafuga";
-#else
+		public static string ApiBodyEncryptKey { private set; get; } = string.Empty;
+
 		/// <summary>
-		/// APIのURL (本番)
+		/// オーバーライドするクラス名
 		/// </summary>
-		public const string ApiHttpRoot = "http://hogehoge.prd/";
+		private const string OverrideClassFullName = "Flappy.WebRequest.WebRequestConstantsOverride";
+
 		/// <summary>
-		/// APIの認証キー
+		/// ゲーム開始時にOverrideクラスの定義をチェックして、定義されていたらOverrideクラスで定義した値を保持しておく
 		/// </summary>
-		public const string ApiAuthKey = "hogehogehoge";
+		[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+		private static void Initialize()
+		{
+			Type overrideClassType = Type.GetType(WebRequestConstants.OverrideClassFullName);
+			if (overrideClassType == null)
+			{
+				return;
+			}
+
+			// 各フィールドの値をオーバーライド
+			WebRequestConstants.ApiHttpRoot = WebRequestConstants.GetStaticPropertyValueFromType(overrideClassType, nameof(ApiHttpRoot));
+			WebRequestConstants.ApiAuthKey = WebRequestConstants.GetStaticPropertyValueFromType(overrideClassType, nameof(ApiAuthKey));
+			WebRequestConstants.ApiBodyEncryptKey = WebRequestConstants.GetStaticPropertyValueFromType(overrideClassType, nameof(ApiBodyEncryptKey));
+		}
+
 		/// <summary>
-		/// APIのボディ暗号化キー
+		/// クラスのstaticプロパティから値を取得
 		/// </summary>
-		public const string ApiBodyEncryptKey = "fugafugafuga";
-#endif
+		private static string GetStaticPropertyValueFromType(Type classType, string fieldName)
+		{
+			var property = classType.GetProperty(fieldName, BindingFlags.Public | BindingFlags.Static);
+			if (property == null || property.PropertyType != typeof(string))
+			{
+				return string.Empty;
+			}
+			return property.GetValue(null).ToString();
+		}
 	}
 }
