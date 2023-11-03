d574fe92bdb8017d1bd30de1d9b7799ac4be5f3f
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2023-11-03 23:19:32 +0900

    ログインしてユーザ名を表示するように

--- Assets/App/Scripts/Scenes/Title/TitleScene.cs
+++ Assets/App/Scripts/Scenes/Title/TitleScene.cs
@@ -1,7 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
+using Flappy.Api;
 using Flappy.Common;
 using Flappy.Manager;
+using TMPro;
 using UnityEngine;
 
 namespace Flappy
@@ -10,6 +10,31 @@ namespace Flappy
 	{
 		public override string Name => "Title";
 
+		[SerializeField]
+		private TextMeshProUGUI playerName;
+
+		[SerializeField]
+		private TextMeshProUGUI playerBestScore;
+
+		[SerializeField]
+		private TextMeshProUGUI playerGems;
+
+		/// <summary>
+		/// 初期化
+		/// </summary>
+		private void Start()
+		{
+			LoadingManager.Instance.Show();
+			new LoginRequest().Request<LoginResponse>((response) =>
+			{
+				this.playerName.text = response.Name;
+				LoadingManager.Instance.CompleteTask();
+			});
+		}
+
+		/// <summary>
+		/// プレイを開始
+		/// </summary>
 		public void StartPlayGame()
 		{
 			SceneManager.Instance.Load<PlayGameScene>();
