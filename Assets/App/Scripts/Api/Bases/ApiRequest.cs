fc30a1265d4663780417db93b587b58e47a81291
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2023-11-03 18:30:48 +0900

    Apiのベースクラスを大幅に修正
    - DictionaryがJson化できないのでJson化可能な代替クラスを用意した
    - 成功時/エラー時のデリゲートを呼ぶように

--- Assets/App/Scripts/Api/Bases/ApiRequest.cs
+++ Assets/App/Scripts/Api/Bases/ApiRequest.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using Flappy.Api;
 using Flappy.Utility;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Networking;
@@ -18,12 +19,12 @@ namespace Flappy.Api
 		/// <summary>
 		/// APIのエンドポイント
 		/// </summary>
-		public string Url { get; }
+		public abstract string Url { get; }
 
 		/// <summary>
 		/// リクエストのパラメータ
 		/// </summary>
-		public Dictionary<string, string> Parameters { get; protected set; }
+		public Dictionary<string, string> Parameters { get; protected set; } = new Dictionary<string, string>();
 
 		/// <summary>
 		/// APIリクエスト実行
@@ -31,30 +32,110 @@ namespace Flappy.Api
 		/// <typeparam name="T">レスポンスの種類</typeparam>
 		/// <param name="onSuccess">成功時のコールバック</param>
 		/// <param name="onError">エラー発生時のコールバック</param>
-		public void Request<T>(UnityAction<T> onSuccess, UnityAction<Exception> onError = null) where T : ApiResponse
+		public virtual void Request<T>(UnityAction<T> onSuccess, UnityAction<Exception> onError = null) where T : ApiResponse, IDisposable, new()
 		{
+			// TODO: ApiManagerを実装する
 			StaticCoroutine.Start(this.SendRequest<T>(onSuccess, onError));
 		}
 
-		private IEnumerator SendRequest<T>(UnityAction<T> onSuccess, UnityAction<Exception> onError = null) where T : ApiResponse
+		private IEnumerator SendRequest<T>(UnityAction<T> onSuccess, UnityAction<Exception> onError = null) where T : ApiResponse, IDisposable, new()
 		{
-			var parametersJson = JsonUtility.ToJson(this.Parameters, false);
-			var encryptedParameter = EncryptionUtility.Encrypt(parametersJson, ApiConstants.BodyEncryptKey);
+			var bodyJson = new RequestBody(this.Parameters).ToJson();
+			var encryptedParameter = EncryptionUtility.Encrypt(bodyJson, ApiConstants.BodyEncryptKey);
 			var url = Path.Combine(ApiConstants.HttpRoot, this.Url);
-			var post = UnityWebRequest.PostWwwForm(url, encryptedParameter);
 
-			yield return post.SendWebRequest();
-
-			switch (post.result)
+			using (var post = UnityWebRequest.PostWwwForm(url, encryptedParameter))
 			{
-				case UnityWebRequest.Result.Success:
-					// TODO: リクエストには成功したがサーバがエラーを返したケースの考慮
-					Debug.Log("APIリクエスト成功");
-					break;
-				default:
-					//TODO: エラー処理
-					Debug.Log("APIリクエストエラー");
-					break;
+				yield return post.SendWebRequest();
+
+				// リクエストの完了を待つ
+				while (post.isDone == false)
+				{
+					yield return null;
+				}
+
+				switch (post.result)
+				{
+					case UnityWebRequest.Result.Success:
+						// responseはFromJsonを実行するためだけのインスタンス
+						// TODO: もっと良いやり方があったら書き直す
+						using (T response = new T())
+						{
+							Debug.Log("APIリクエスト成功: " + post.downloadHandler.text);
+							onSuccess?.Invoke(response.FromJson(post.downloadHandler.text) as T);
+						}
+						break;
+					default:
+						//TODO: エラーコードを返す
+						//TODO: onError 設定していない時用の共通エラー処理
+						Debug.Log("APIリクエストエラー: " + post.downloadHandler.text);
+						onError?.Invoke(new Exception(post.downloadHandler.text));
+						break;
+				}
+			}
+		}
+
+		/// <summary>
+		/// リクエストのボディに使用する
+		/// </summary>
+		[Serializable]
+		private class RequestBody
+		{
+			/// <summary>
+			/// API認証キー
+			/// </summary>
+			public string ApiAuthKey = ApiConstants.AuthKey;
+			/// <summary>
+			/// リクエストパラメータ
+			/// </summary>
+			public SerializableDictionary<string, string> Parameter;
+
+			public RequestBody(Dictionary<string, string> parameter)
+			{
+				this.Parameter = new SerializableDictionary<string, string>();
+				this.Parameter.AddRange(parameter);
+			}
+
+			public string ToJson()
+			{
+				return JsonUtility.ToJson(this, false);
+			}
+		}
+
+		/// <summary>
+		/// Jsonシリアライズ可能なDictionary代替クラス
+		/// </summary>
+		/// <remarks>サーバサイドでは受け取ってから連想配列に変換している</remarks>
+		[Serializable]
+		public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
+		{
+			[SerializeField]
+			private List<TKey> keys = new List<TKey>();
+			[SerializeField]
+			private List<TValue> values = new List<TValue>();
+
+			public void OnBeforeSerialize()
+			{
+				keys.Clear();
+				values.Clear();
+
+				var e = GetEnumerator();
+
+				while (e.MoveNext())
+				{
+					keys.Add(e.Current.Key);
+					values.Add(e.Current.Value);
+				}
+			}
+
+			public void OnAfterDeserialize()
+			{
+				this.Clear();
+
+				int cnt = (keys.Count <= values.Count) ? keys.Count : values.Count;
+				for (int i = 0; i < cnt; ++i)
+					this[keys[i]] = values[i];
+
 			}
 		}
 	}
--- Assets/App/Scripts/Api/Bases/ApiResponse.cs
+++ Assets/App/Scripts/Api/Bases/ApiResponse.cs
@@ -1,10 +1,32 @@
+using System;
+using UnityEngine;
+
 namespace Flappy.Api
 {
 	/// <summary>
 	/// APIレスポンスのベースクラス
 	/// </summary>
-	public abstract class ApiResponse : IApiResponse
+	public abstract class ApiResponse : IApiResponse, IDisposable
 	{
+		/// <summary>
+		/// Jsonからインスタンスを生成
+		/// </summary>
+		/// <remarks>
+		/// virtualにして必要に応じてoverrideしたいのでstaticにはできない
+		/// もし今後のバージョンアップでC# 11.0以上になったらできる
+		/// </remarks>
+		public virtual ApiResponse FromJson(string json)
+		{
+			return JsonUtility.FromJson<ApiResponse>(json);
+		}
 
+		/// <summary>
+		/// インスタンスを破棄
+		/// </summary>
+		/// <remarks>必要に応じてオーバーライドして実装する</remarks>
+		public virtual void Dispose()
+		{
+			// Nothind to do.
+		}
 	}
 }
