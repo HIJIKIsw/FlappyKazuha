a2fc98230b4e64149eb336a334a485302868d900
HIJIKIsw <3005814+HIJIKIsw@users.noreply.github.com>
2023-11-05 19:32:14 +0900

    タイトル画面でユーザの最高記録を取得する処理を追加

--- Assets/App/Scripts/Scenes/Title/TitleScene.cs
+++ Assets/App/Scripts/Scenes/Title/TitleScene.cs
@@ -17,6 +17,18 @@ namespace Flappy
 		private TextMeshProUGUI playerName;
 
 		/// <summary>
+		/// プレイヤー最高スコア
+		/// </summary>
+		[SerializeField]
+		private TextMeshProUGUI playerBestScore;
+
+		/// <summary>
+		/// プレイヤー最高原石獲得数
+		/// </summary>
+		[SerializeField]
+		private TextMeshProUGUI playerBestGemScore;
+
+		/// <summary>
 		/// プレイヤー累計スコア
 		/// </summary>
 		[SerializeField]
@@ -33,16 +45,29 @@ namespace Flappy
 		/// </summary>
 		private void Start()
 		{
-			LoadingManager.Instance.Show();
+			// ログイン
+			LoadingManager.Instance.Show(LoadingManager.Types.Overlay, 3);
 			new LoginRequest().Request<LoginResponse>((loginResponse) =>
 			{
 				GameManager.Instance.UserId = loginResponse.UserId;
+				this.playerName.text = loginResponse.Name;
+				LoadingManager.Instance.CompleteTask();
+
+				// 最高記録の取得
+				new UserRankingRequest(loginResponse.UserId, UserRankingRequest.Division.Overall).Request<UserRankingResponse>((rankingResponse) =>
+				{
+					GameManager.Instance.BestScore = rankingResponse.Score;
+					this.playerBestScore.text = GameManager.Instance.ScoreToText(GameManager.Instance.BestScore);
+					this.playerBestGemScore.text = rankingResponse.GemScore.ToString();
+					LoadingManager.Instance.CompleteTask();
+				});
+
+				// 統計情報の取得
 				new UserStatsRequest(loginResponse.UserId).Request<UserStatsResponse>((statsResponse) =>
 				{
 					GameManager.Instance.AccumulatedScore = statsResponse.AccumulatedScore;
 					GameManager.Instance.AccumulatedGemScore = statsResponse.AccumulatedGemScore;
 
-					this.playerName.text = loginResponse.Name;
 					this.playerAccumulatedScore.text = GameManager.Instance.ScoreToText(statsResponse.AccumulatedScore);
 					this.playerAccumulatedGemScore.text = statsResponse.AccumulatedGemScore.ToString();
 					LoadingManager.Instance.CompleteTask();
