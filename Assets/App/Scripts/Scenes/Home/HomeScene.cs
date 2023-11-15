42b2d884bf959b589bbde5f890ce6dabd781e4ba
NV <ghostrecon.kara@gmail.com>
2023-11-16 01:59:07 +0900

    キャラクターの立ち絵とImageオブジェクトを追加
    未実装の胡桃も追加

--- Assets/App/Scripts/Scenes/Home/HomeScene.cs
+++ Assets/App/Scripts/Scenes/Home/HomeScene.cs
@@ -2,12 +2,13 @@ using Flappy.Common;
 using Flappy.Manager;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Flappy
 {
-    public class HomeScene : SceneBase
-    {
-        public override string Name => "Home";
+	public class HomeScene : SceneBase
+	{
+		public override string Name => "Home";
 
 		/// <summary>
 		/// CharacterListオブジェクト
@@ -15,41 +16,63 @@ namespace Flappy
 		[SerializeField]
 		private TMP_Dropdown characterList;
 
-        /// <summary>
-        /// Homeシーンで「開始」ボタンを押した時に実行される
-        /// </summary>
-        public void StartPlayGame()
-        {
-            // シーンに渡すパラメータを生成
-            SceneParameter parameter = new SceneParameter();
+		[SerializeField]
+		private Image characterImage;
 
-            // 選択されたキャラクターに応じてパラメータに値をセット
-            switch (characterList.value)
-            {
-                case 0:
-                    {
-                        parameter.Add(PlayGameScene.Parameters.Character, Constants.Assets.Prefab.Player.Kazuha);
-                        break;
-                    }
-                case 1:
-                    {
-                        Debug.Log (parameter);
-                        parameter.Add(PlayGameScene.Parameters.Character, Constants.Assets.Prefab.Player.Wanderer);
-                        break;
-                    }
-                default:
-                    {
-                        Debug.Log (parameter);
-                        parameter.Add(PlayGameScene.Parameters.Character, Constants.Assets.Prefab.Player.Kazuha);
-                        break;
-                    }
-            }
+		/// <summary>
+		/// 初期化
+		/// </summary>
+		private void Start()
+		{
+			// キャラ変更時に呼び出されるイベントを登録
+			this.characterList.onValueChanged.AddListener(this.OnChangeCharacter);
+		}
 
-            // PlayGameSceneにパラメータを渡す
-            SceneManager.Instance.Load<PlayGameScene>(parameter);
-        }
+		/// <summary>
+		/// キャラ変更時のイベント
+		/// </summary>
+		/// <param name="characterIndex">選択されたキャラクターの番号</param>
+		private void OnChangeCharacter(int characterIndex)
+		{
+			// TODO: 固定でnullをセットしているところを、characterIndexの値に応じて変える (対応するキャラクターのSpriteをセットする)
+			this.characterImage.sprite = null;
+		}
 
-    }
+		/// <summary>
+		/// Homeシーンで「開始」ボタンを押した時に実行される
+		/// </summary>
+		public void StartPlayGame()
+		{
+			// シーンに渡すパラメータを生成
+			SceneParameter parameter = new SceneParameter();
+
+			// 選択されたキャラクターに応じてパラメータに値をセット
+			switch (characterList.value)
+			{
+				case 0:
+					{
+						parameter.Add(PlayGameScene.Parameters.Character, Constants.Assets.Prefab.Player.Kazuha);
+						break;
+					}
+				case 1:
+					{
+						Debug.Log(parameter);
+						parameter.Add(PlayGameScene.Parameters.Character, Constants.Assets.Prefab.Player.Wanderer);
+						break;
+					}
+				default:
+					{
+						Debug.Log(parameter);
+						parameter.Add(PlayGameScene.Parameters.Character, Constants.Assets.Prefab.Player.Kazuha);
+						break;
+					}
+			}
+
+			// PlayGameSceneにパラメータを渡す
+			SceneManager.Instance.Load<PlayGameScene>(parameter);
+		}
+
+	}
 
 
 }
